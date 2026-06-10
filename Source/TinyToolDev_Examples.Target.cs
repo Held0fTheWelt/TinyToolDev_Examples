6b65b495ab5faec865dcd315f70f2ddcdbd7ed11
Yves Tanas <Yves.Tanas@gmx.de>
2026-06-10 14:02:43 +0200

    Update TinyToolDev_Examples.Target.cs

--- Source/TinyToolDev_Examples.Target.cs
+++ Source/TinyToolDev_Examples.Target.cs
@@ -9,7 +9,7 @@ public class TinyToolDev_ExamplesTarget : TargetRules
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
 		ExtraModuleNames.Add("TinyToolDev_Examples");
 	}
 }
